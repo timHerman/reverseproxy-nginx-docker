f7c02c17e2802c89beacd841974514f72bc6aadf
Tim Herman <tim@belg.be>
2014-07-15 23:20:54 +0200

    added redirect and logging

--- template/nginx.conf.tpl
+++ template/nginx.conf.tpl
@@ -2,9 +2,11 @@ server {
 	#ssl_certificate /etc/nginx/certs/demo.pem;
 	#ssl_certificate_key /etc/nginx/certs/demo.key;
 
-	server_name %(hostname)s;
+	server_name www.%(hostname)s;
 
 	location / {
-	    proxy_pass http://%(ipaddress)s;
+		proxy_set_header Host $host;
+		proxy_set_header X-Real-IP $remote_addr;
+		proxy_pass http://%(ipaddress)s;
 	}
 }
--- test.py
+++ test.py
@@ -1,4 +1,13 @@
-import docker, os, glob
+#!/usr/bin/env python
+
+import docker, os, logging, logging.handlers
+
+LOG_FILENAME = 'generater.log'
+logger = logging.getLogger('generator')
+logger.setLevel(logging.DEBUG)
+
+handler = logging.handlers.RotatingFileHandler(LOG_FILENAME, maxBytes=1048576, backupCount=5)
+logger.addHandler(handler)
 
 def get_router_virtual_host(detailList):
 	env = detailList["Config"]["Env"]
@@ -35,6 +44,36 @@ def get_router_virtual_cert(detailList):
 def get_ip_address(detailList):
 	return detailList["NetworkSettings"]["IPAddress"]
 
+def removeOldFiles(dir, generatedFileNames):
+	for (_, __, files) in os.walk(dir):
+		for file in files:
+			if file.startswith('generated.'):
+				fullPathFile = 'output/%s' % file
+				if not fullPathFile in generatedFileNames:
+					logger.debug('Removing old file %s' % fullPathFile)
+					os.remove(fullPathFile)
+
+def writeFile(filePath, output):
+	outputFile = open(filePath, 'w+')
+	outputFile.write(output)
+	outputFile.close()
+	logger.debug('Add new file %s' % filePath)
+
+def generateTemplate(templateFile, filePath, host_name, host_ip, host_cert, host_port):
+	# Load template and fill with data
+	templateFile = open(templateFile,'r')
+	output = templateFile.read() % ( { "hostname": host_name, "ipaddress": host_ip, "cert": host_cert, "port": host_port    } )
+
+	# Check if file exists, if there are no changes, do nothing (We want to detect changes later on to reload nginx)
+	if os.path.isfile(filePath):
+		existingFile = open(filePath, 'r')
+		fileContent = existingFile.read()
+		if fileContent != output:
+			writeFile(filePath, output)
+	else:
+		writeFile(filePath, output)
+
+
 def main():
 	c = docker.Client(base_url='unix://var/run/docker.sock',
 			  version='1.12',
@@ -47,46 +86,35 @@ def main():
 
 	for container in containerList:
 		detailList = c.inspect_container(container["Id"])
-		print "IP: %s, router_virtual_host: %s" % ( get_ip_address(detailList), get_router_virtual_host(detailList) )
 
 		host_ip = get_ip_address(detailList)
 		host_name = get_router_virtual_host(detailList)
 		host_cert = get_router_virtual_cert(detailList)
 		host_port = get_router_virtual_port(detailList)
 
+		logger.debug('Handling container IP %s & VHost %s' % ( host_ip, host_name ))
+
+		if host_name and host_name.startswith('www.'):
+			host_name = host_name[4:]
+
 		# Check if the mimimum is set
 		if host_ip and host_name:
-			# Load template and fill with data
-			templateFile = open('template/nginx.conf.tpl','r')
-			output = templateFile.read() % ( { "hostname": host_name, "ipaddress": host_ip, "cert": host_cert, "port": host_port    } )
-			
-			# Generate filename
-			filePath = 'output/generated.%(filename)s.conf' % ( { "filename": host_name } )
+			filePath = 'output/generated.%s.conf' % host_name
+			generateTemplate('template/nginx.conf.tpl', filePath, host_name, host_ip, host_cert, host_port)
 			generatedFileNames.append(filePath)
-			# Check if file exists, if there are no changes, do nothing (We want to detect changes later on to reload nginx)
-			if os.path.isfile(filePath):
-				existingFile = open(filePath, 'r')
-				fileContent = existingFile.read()
-				if fileContent != output:
-					writeFile(filePath, output)
-			else:
-				writeFile(filePath, output)
+
+			filePath = 'output/generated.redirect.%s.conf' % host_name
+			generateTemplate('template/redirect.nginx.conf.tpl', filePath, host_name, host_ip, host_cert, host_port)
+			generatedFileNames.append(filePath)
 
 	removeOldFiles('output', generatedFileNames)
-
-def removeOldFiles(dir, generatedFileNames):
-	for (_, __, files) in os.walk(dir):
-		for file in files:
-			if file.startswith('generated.'):
-				fullPathFile = 'output/%s' % file
-				if not fullPathFile in generatedFileNames:
-					print "Removing old file %s" % fullPathFile
-					os.remove(fullPathFile)
-
-def writeFile(filePath, output):
-	outputFile = open(filePath, 'w+')
-	outputFile.write(output)
-	outputFile.close()
+			
 
 if __name__ == "__main__":
 	main()
+
+"""
+
+SCRAPZONE
+
+"""
